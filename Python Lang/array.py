4c4d8578476901e04156fc80d2641f8cc39d2c4f
davian27 <daviannaufalfadhil@gmail.com>
2024-04-27 11:52:21 +0700

    Updated Array & String Python

---
+++ Python Lang/array.py
@@ -0,0 +1,4 @@
+buah = ["apel","mangga","pisang"]
+
+print(len(buah))
+print(buah[1])
---
+++ Python Lang/string.py
@@ -0,0 +1,24 @@
+def normal_string ():
+    print("hello world")
+
+def slicing_string ():
+    i = "hello world"
+    print(i[2:5])
+
+def modify_string ():
+    i = "hello world"
+    print(i.upper())
+    print(i.lower())
+
+def string_concatenation ():
+    a = "hello"
+    b = "world"
+    c = a + b
+    print(c)
+
+string_concatenation ()
+
+
+
+
+
